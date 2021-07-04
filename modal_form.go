7d95baf1653af02834223c62c05548749a4a8620
Eric Stokes <fernferret@gmail.com>
2021-07-04 14:42:12 +0100

    Rework the way styles are applied to dialogs
    After discussions with @derailed, this gets rid of the need for a new
    member variable and the global by just applying styles via SetStyle.

--- modal_form.go
+++ modal_form.go
@@ -4,15 +4,9 @@ import (
 	"github.com/gdamore/tcell/v2"
 )
 
-var defaultStyle *ModalStyleOpts
-
 // ModalForm implements a modal window with a custom form.
 type ModalForm struct {
 	*Modal
-}
-
-func SetModalStyle(opts *ModalStyleOpts) {
-	defaultStyle = opts
 }
 
 // NewModalForm implements a modal that can take in a custom form.
@@ -36,14 +30,17 @@ func NewModalForm(title string, form *Form) *ModalForm {
 	return &m
 }
 
-func (m *ModalForm) setStyle(style *ModalStyleOpts) {
-
+// SetStyle sets the style of the ModalForm and by setting the style settings
+// for the underlying Frame and Form
+func (m *ModalForm) SetStyle(style *ModalStyleOpts) *ModalForm {
 	// Make sure to set both the frame and the form. I don't think we need
 	// to differentiate between the two as it almost always looks awkward if you
 	// do.
 	m.frame.SetBackgroundColor(style.BgColor)
 	m.form.SetBackgroundColor(style.BgColor)
-	// If transparent set to the color of the form bg
+
+	// If the field's BGColor is transparent, set to the color of the form bg so
+	// it will blend nicely.
 	if style.FieldBgColor == tcell.ColorDefault {
 		m.form.SetFieldBackgroundColor(style.BgColor)
 	} else {
@@ -51,17 +48,15 @@ func (m *ModalForm) setStyle(style *ModalStyleOpts) {
 	}
 
 	m.form.SetFieldTextColor(style.FieldFgColor)
-	m.form.SetTitleColor(style.TitleFgColor)
 	m.form.SetLabelColor(style.LabelFgColor)
 
-	setButtonStyle(m.form, style)
-}
+	// The TitleColor only matters on the frame, as the form within the frame
+	// does not have a title.
+	m.frame.SetTitleColor(style.TitleFgColor)
 
-// SetButtonStyle takes a Forma and a dialog config and ensures sure all buttons
-// in the form have the same look and feel, via the theme engine.
-func setButtonStyle(f *Form, style *ModalStyleOpts) {
-	for i := 0; i < f.GetButtonCount(); i++ {
-		b := f.GetButton(i)
+	// Style all buttons the same to get a common look and feel.
+	for i := 0; i < m.form.GetButtonCount(); i++ {
+		b := m.form.GetButton(i)
 		if b == nil {
 			continue
 		}
@@ -70,19 +65,11 @@ func setButtonStyle(f *Form, style *ModalStyleOpts) {
 		b.SetBackgroundColorActivated(style.ButtonFocusBgColor)
 		b.SetLabelColorActivated(style.ButtonFocusFgColor)
 	}
+	return m
 }
 
-// Draw draws a modal styled with the default style
+// Draw draws this primitive onto the screen.
 func (m *ModalForm) Draw(screen tcell.Screen) {
-	m.DrawWithStyle(defaultStyle, screen)
-}
-
-// DrawWithStyle draws a modal with a custom style. This is useful for error
-// dialogs or other places where the default style is not acceptable.
-func (m *ModalForm) DrawWithStyle(style *ModalStyleOpts, screen tcell.Screen) {
-	if style != nil {
-		m.setStyle(style)
-	}
 	// Calculate the width of this modal.
 	buttonsWidth := 0
 	for _, button := range m.form.buttons {
